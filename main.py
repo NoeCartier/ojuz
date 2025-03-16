4cb599eec4780386150ad52cc37523681c993772
Noé Cartier <noe.cartier@laposte.net>
2025-03-16 18:30:10 +0100

    Refactor leaderboard display and add manual submission processing command

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import pytz
 
 
 #LOCAL
-"""
+
 GUILD_ID           = 1117791942764396707
 TRAINER_ROLE_ID    = 1341179125271887982
 POTW_CHANNEL       = 1341182372921610281
@@ -29,7 +29,7 @@ LOG_CHANNEL        = -1
 POTW_ROLE_ID       = 1345134997287469137
 MY_ID              = 468810262263365643
 AC_ROLE_ID         = -1
-
+"""
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 
@@ -62,13 +62,25 @@ async def show_leaderboard():
         return
     
     leaderboard.sort(key=lambda x: x[1], reverse=True)
-    embed = discord.Embed(title=f"Leaderboard for Problem #{potw_number}", color=0xFFD700)
+    embed = discord.Embed(title=f"Leaderboard for Problem #{potw_number}", color=0x18191C)
     
+    rank = 1
+    prev_score = None
     for i, entry in enumerate(leaderboard):
         user_id, score = entry
-        user = await bot.fetch_user(user_id)
-        embed.add_field(name=f"{i+1}. {user.display_name}", value=f"Score: {score * 100}%", inline=False)
-    
+        if prev_score is None or score < prev_score:
+            rank = i + 1
+        try:
+            member = await bot.get_guild(GUILD_ID).fetch_member(user_id)
+        except discord.NotFound:
+            await log(f"Member with ID {user_id} not found.")
+            continue
+        embed.add_field(
+            name=f"~{rank}. **{member.display_name}** : {score * 100}%",
+            value="",
+            inline=False
+        )
+        prev_score = score
     channel = await bot.fetch_channel(POTW_CHANNEL)
     await channel.send(embed=embed)
     await channel.send(f"<@&{POTW_ROLE_ID}>")
@@ -107,8 +119,9 @@ async def postPOTW():
     try:
         proposer_user = await bot.fetch_user(problem_proposer)  # Fetch user globally
         avatar_url = proposer_user.avatar.url if proposer_user.avatar else proposer_user.default_avatar.url
+        member = await bot.get_guild(GUILD_ID).fetch_member(proposer_user.id)
         embed.set_footer(
-            text=f"Proposed by : {proposer_user.display_name}",
+            text=f"Proposed by : {member.display_name}",
             icon_url=avatar_url
         )
         await log(f"The username for ID {problem_proposer} is {proposer_user.name}")
@@ -144,7 +157,34 @@ async def postPOTW():
     db.add_problem_thread(problem_id, thread.id)
     await log(f"Thread ID {thread.id} for problem {problem_id} stored in the database.")
 
-    
+async def process_submission(submission_id, submission_time, user_id, problem_id, score):
+
+    if user_id:
+        await log("User found")
+        best_score = db.check_best_score_of_user(user_id, problem_id)
+        db.add_submission(submission_id, score, user_id, problem_id)
+        await log("Submission added")
+        if score > best_score:
+            await log("New best score")
+            if problem_id == db.get_variable_value("potw_problem_id"):
+                db.add_to_leaderboard(user_id, score)
+            thread_id = db.get_thread_id_of_problem(problem_id)
+            if thread_id:
+                await log("Thread found")
+                if score == 1.0:
+                    await chat(thread_id, f":green_square: <t:{submission_time}:T> | <@{user_id}> solved the problem !")
+                    guild = bot.get_guild(GUILD_ID)
+                    member = guild.get_member(user_id)
+                    if AC_ROLE_ID != -1:
+                        role = guild.get_role(AC_ROLE_ID)
+                        if member and role:
+                            await member.add_roles(role)
+                            await log(f"Added AC role to {member.display_name}")
+                elif score == 0.0:
+                    await chat(thread_id, f":red_square: <t:{submission_time}:T> | <@{user_id}> attempted the problem without success.")
+                else:
+                    await chat(thread_id, f":yellow_square: <t:{submission_time}:T> | <@{user_id}> scored {score * 100} !")
+
 async def upd():
     users = db.list_all_users()
     submissions = update.update_submissions(users)
@@ -157,37 +197,11 @@ async def upd():
         problem_name = submissions[iSub][3]
         language = submissions[iSub][4]
         score = submissions[iSub][5]
-
         user_id = db.get_user_id_by_ojuz_handle(ojuz_handle)
         result = re.search(r'\((.*?)\)', problem_name)
         problem_code = result.group(1) if result else None
         problem_id = db.get_problem_id_by_code(problem_code)
-        if(user_id):
-            await log("User found")
-            best_score = db.check_best_score_of_user(user_id, problem_id)
-            db.add_submission(submission_id, score, user_id, problem_id)
-            await log("Submission added")
-            if score > best_score:
-                await log("New best score")
-                if problem_id == db.get_variable_value("potw_problem_id"):
-                    db.add_to_leaderboard(user_id, score)
-                thread_id = db.get_thread_id_of_problem(problem_id)
-                if thread_id:
-                    await log("Thread found")
-                    #thread = await bot.fetch_channel(thread_id)
-                    if(score == 1.0):
-                        await chat(thread_id, f":green_square: <t:{submission_time}:T> | <@{user_id}> solved the problem !")
-                        guild = bot.get_guild(GUILD_ID)
-                        member = guild.get_member(user_id)
-                        if(AC_ROLE_ID != -1):
-                            role = guild.get_role(AC_ROLE_ID)
-                            if member and role:
-                                await member.add_roles(role)
-                                await log(f"Added AC role to {member.display_name}")
-                    elif(score == 0.0) :
-                        await chat(thread_id, f":red_square: <t:{submission_time}:T> | <@{user_id}> attempted the problem without success.")
-                    else:
-                        await chat(thread_id, f":yellow_square: <t:{submission_time}:T> | <@{user_id}> scored {score * 100} !")
+        await process_submission(submission_id, submission_time, user_id, problem_id, score)
     await log("Update done.")
 
 @bot.event
@@ -253,7 +267,20 @@ async def add_problem(ctx, problem_code: str, problem_title: str, problem_priori
     else:
         await ctx.send(f'An error occurred while adding the problem. Please try again.')
 
-
+@bot.command(name='submit', description="Manually process a submission")
+async def manual_process_submission(ctx, score: float = 1.0):
+    if score < 0.0 or score > 1.0:
+        await ctx.send("Error: Score must be between 0.0 and 1.0")
+        return
+    user_id = ctx.author.id
+    problem_id = db.get_variable_value("potw_problem_id")
+    if problem_id <= 825:
+        await ctx.send("Error: Please submit on the oj.uz website instead.")
+        return
+    submission_id = -1 # Use the current timestamp as submission ID
+    submission_time = int(datetime.datetime.now().timestamp())  # Current timestamp
+    await process_submission(submission_id, submission_time, user_id, problem_id, score)
+    await ctx.send(f"Submission processed.")
 
 
 # Scheduled task to run every monday at 7:42 AM
--- test.py
+++ test.py
@@ -1,8 +1,1 @@
 import db
-
-
-db.update_problem_priority(50, -1)
-db.update_problem_priority(257, 9000)
-
-db.add_user(739395682749907015, "raphaelp")
-
